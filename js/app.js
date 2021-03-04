45facd7b808aad95b63f55fd53c37abf5652fac4
maryna1408 <cabanmarina9@gmail.com>
2021-03-04 22:45:36 +0200

    sortMessages + ModalWindow

--- js/app.js
+++ js/app.js
@@ -24,58 +24,42 @@ const timeFormatter = new Intl.DateTimeFormat(undefined, {
 
 
 cardListEl.addEventListener('click', event => {
-    const messageEl = event.target.closest('.massage')
+    const messageEl = event.target.closest('.message')
     if (messageEl) {
-        
+        const messageId = messageEl.dataset.id
+        MESSAGES.forEach((message, i, array) => {
+            if (message.id == messageId) {
+                if (message.seen) {
+                    array.splice(i,1)
+                } else {
+                    message.seen = true
+                }
+            }
+        })
+        RenderCards(MESSAGES, cardListEl)
     }
 })
 
 
 
-
-countAllMessages(MESSAGES, messagesNumEl)
-
-function countAllMessages(data_array, num) {
-    let sum = ''
-    sum = data_array.length
-    num.insertAdjacentHTML('beforeend', sum)
-}
-
-
-
-countNotReadMessages(MESSAGES, notReadEl)
-
-function countNotReadMessages(data_array, num) {
-    let NotReadMessage = []
-    
-    MESSAGES.forEach(input => {
-        if (!input.seen) {
-            NotReadMessage.push(input.id)
-        }
+formSearchEl.addEventListener('submit', function (event) {
+    event.preventDefault()
+    const query = this.search.value.trim().toLowerCase().split(' ').filter(word => !!word)
+    const searchFields = ['name', 'phone', 'message']
+    MESSAGES = JSON.parse(DATA).filter(message => {
+      return query.every(word => {
+        return searchFields.some(field => {
+          return `${message[field]}`.trim().toLowerCase().includes(word)
+        })
+      })
     })
-
-    num.insertAdjacentHTML('beforeend', NotReadMessage.length)
-}
-
-
-
-// formSearchEl.addEventListener('submit', function (event) {
-//     event.preventDefault()
-//     const query = this.search.value.trim().toLowerCase().split(' ').filter(word => !!word)
-//     const searchFields = ['name', 'phone']
-//     MESSAGES = JSON.parse(DATA).filter(message => {
-//       return query.every(word => {
-//         return searchFields.some(field => {
-//           return `${message[field]}`.trim().toLowerCase().includes(word)
-//         })
-//       })
-//     })
-//     renderCards(MESSAGES, cardListEl)
-//   })
+    RenderCards(MESSAGES, cardListEl)
+  })
 
 
 
 renderBtnEl.addEventListener('click', () => {
+    MESSAGES = JSON.parse(DATA)
     RenderCards(MESSAGES, cardListEl)
 })
 
@@ -85,6 +69,13 @@ RenderCards(MESSAGES, cardListEl)
 function RenderCards(data_array, node) {
     let html = ''
 
+    messagesNumEl.textContent = data_array.length
+    notReadEl.textContent = data_array.filter(message => !message.seen).length
+
+    data_array.sort((a,b) => {
+        return a.seen - b.seen || b.date - a.date
+    })
+
     data_array.forEach(el => html += CreateCardHTML(el));
 
     node.innerHTML = html
@@ -92,7 +83,7 @@ function RenderCards(data_array, node) {
 
 function CreateCardHTML(card_data) {
 
-    return `<div class="message row g-0 pb-4 px-3 d-flex align-items-center h-auto ${!card_data.seen ? 'not-read-bg fw-bold' : ''}">
+    return `<div class="message row g-0 pb-4 px-3 d-flex align-items-center h-auto ${!card_data.seen ? 'not-read-bg fw-bold' : ''}" data-id="${card_data.id}">
     <div class="col-4 sender d-flex justify-content-start align-items-center">
     <img class="avatar" width="1" height="1" loading="lazy" src="${card_data.avatar}" alt="${card_data.name}">
     <div class="pers-info text-center">
@@ -100,7 +91,7 @@ function CreateCardHTML(card_data) {
         <div class="phone">${card_data.phone}</div>
     </div>
 </div>
-<div class="col-4 message">${card_data.message}</div>
+<div class="col-4 text">${card_data.message}</div>
 <div class="col-4 date d-flex justify-content-end">
     <div class="date">${dateFormatter.format(card_data.date)} ${timeFormatter.format(card_data.date)}</div>
 </div>
@@ -108,4 +99,22 @@ function CreateCardHTML(card_data) {
 } 
 
 
+
+
+
+let arr = [{price: 100},{price: 2000},{price:50 },{price: 150},{price: 500}]
+
+arr.sort((a,b) => {
+    return a.price - b.price
+})
+
+console.log(arr);
+
+
+const user = {
+    name: 'Marina',
+    age: 16
+}
+
+console.log(user.age);
 
