4652181f800da7ed939a885829e34118ed39205b
maryna1408 <cabanmarina9@gmail.com>
2021-03-11 15:58:51 +0200

    ModalWindow

--- js/app.js
+++ js/app.js
@@ -4,6 +4,10 @@ const renderBtnEl = document.getElementById('renderBtn')
 const messagesNumEl = document.getElementById('messagesNum')
 const notReadEl = document.getElementById('notRead')
 const formSearchEl = document.getElementById('formSearch')
+const modalWindow = document.getElementById("modalWindow")
+const openModalBtn = document.getElementById("openModalBtn")
+const btnReplyEL = document.getElementById("btnReply")
+const formControlEl = document.getElementById("floatingTextarea2")
 
 const dateFormatter = new Intl.DateTimeFormat()
 const timeFormatter = new Intl.DateTimeFormat(undefined, {
@@ -21,25 +25,38 @@ const timeFormatter = new Intl.DateTimeFormat(undefined, {
 //     "seen": false
 //   }
 
-
+btnReplyEL.addEventListener('click', () => {
+  console.log(formControlEl.value)
+  document.body.classList.remove('modal-open1')
+  formControlEl.value = null
+})
 
 cardListEl.addEventListener('click', event => {
     const messageEl = event.target.closest('.message')
     if (messageEl) {
         const messageId = messageEl.dataset.id
-        MESSAGES.forEach((message, i, array) => {
+        MESSAGES.forEach((message) => {
             if (message.id == messageId) {
-                if (message.seen) {
-                    array.splice(i,1)
-                } else {
-                    message.seen = true
-                }
+                message.seen = true
+                document.body.classList.add('modal-open1')
+                createModalWindow(MESSAGES)
             }
         })
         RenderCards(MESSAGES, cardListEl)
     }
 })
 
+modalWindow.addEventListener("click", function (event) {
+    if (event.target == this || event.target.closest('.modal-close1')) {
+      document.body.classList.remove('modal-open1')
+    }
+})
+
+document.addEventListener("keyup", event => {
+    if (event.key === 'Escape') {
+      document.body.classList.remove('modal-open1')
+    }
+})
 
 
 formSearchEl.addEventListener('submit', function (event) {
@@ -98,23 +115,58 @@ function CreateCardHTML(card_data) {
     </div>`
 } 
 
+// function renderModalWindow(data_array, node) {
+//   let html = ''
 
+//   data_array.forEach(el => html + createModalWindow(el));
 
+//     node.innerHTML = html
+// }
 
-
-let arr = [{price: 100},{price: 2000},{price:50 },{price: 150},{price: 500}]
-
-arr.sort((a,b) => {
-    return a.price - b.price
-})
-
-console.log(arr);
-
-
-const user = {
-    name: 'Marina',
-    age: 16
+function createModalWindow(card_data) {
+    return `<div class="modal1">
+    <div class="modal-header1">
+      <div class="sender d-flex justify-content-start align-items-center">
+        <img class="avatar" width="1" height="1" loading="lazy" src="https://robohash.org/repellendusimpeditnisi.png?size=50x50&set=set1" alt="">
+        <div class="pers-info">
+            <div class="name mb-1">${card_data.name}</div>
+            <div class="phone">${card_data.phone}</div>
+        </div>
+        <div class="col-4 date d-flex justify-content-end">
+          <div class="date">${card_data.date}</div>
+      </div>
+      <button class="modal-close1 btn btn-secondary"><i class="fas fa-times"></i></button>
+    </div>
+    <div class="modal-body1 my-4">
+    ${card_data.message}
+    </div>
+    <div class="row modal-footer">
+      <div class="col-12 g-0 mx-0 form-floating mb-2">
+        <textarea class="form-control" placeholder="Leave a comment here" id="floatingTextarea2" style="height: 70px"></textarea>
+        <label for="floatingTextarea2">Your reply ...</label>
+      </div>
+      <div class="d-flex justify-content-center g-0 mx-0">
+        <button class="btReply btn btn-primary text-white mx-0 ">Send</button>
+      </div>
+    </div>
+  </div>`
 }
 
-console.log(user.age);
+
+
+// let arr = [{price: 100},{price: 2000},{price:50 },{price: 150},{price: 500}]
+
+// arr.sort((a,b) => {
+//     return a.price - b.price
+// })
+
+// console.log(arr);
+
+
+// const user = {
+//     name: 'Marina',
+//     age: 16
+// }
+
+// console.log(user.age);
 
